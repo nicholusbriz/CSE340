9c56355fd733adca0e6a04ae2a704558bb04511c
nicholusbriz <atbriz256@gmail.com>
2026-01-20 14:10:44 +0300

    Week 2 work

---
+++ database/assignment2.sql
@@ -0,0 +1,29 @@
+-- 1. Insert Tony Stark into the account table
+INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password)
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+-- 2. Update Tony Stark's account_type to Admin
+UPDATE public.account
+SET account_type = 'Admin'
+WHERE account_email = 'tony@starkent.com';
+
+-- 3. Delete Tony Stark's record
+DELETE FROM public.account
+WHERE account_email = 'tony@starkent.com';
+
+-- 4. Update GM Hummer description using REPLACE()
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
+WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+
+-- 5. Inner join to show Sport vehicles
+SELECT inv_make, inv_model, classification_name
+FROM public.inventory
+INNER JOIN public.classification
+ON inventory.classification_id = classification.classification_id
+WHERE classification_name = 'Sport';
+
+-- 6. Update image paths to include "/vehicles"
+UPDATE public.inventory
+SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
+    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
