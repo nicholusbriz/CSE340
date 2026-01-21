370d40fa029c1305eedc77bb02252fea60fe26ca
nicholusbriz <atbriz256@gmail.com>
2026-01-21 14:24:17 +0300

    vehicle detail page

--- utilities/index.js
+++ utilities/index.js
@@ -36,7 +36,7 @@ Util.buildClassificationGrid = async function(data){
       grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
       + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
+      +'" alt="Image of '+ vehicle.inv_year + ' '+ vehicle.inv_make + ' ' + vehicle.inv_model 
       +' on CSE Motors" /></a>'
       grid += '<div class="namePrice">'
       grid += '<hr />'
