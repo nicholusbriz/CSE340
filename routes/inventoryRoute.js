210967202700198071dc68d9e50cb2f0ff96dd0b
nicholusbriz <atbriz256@gmail.com>
2026-01-26 11:06:54 +0300

    Vehicle detail page

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,12 +1,18 @@
-const express = require("express")
-const router = new express.Router() 
-const invController = require("../controllers/invController")
-const utilities = require('../utilities')
+const express = require("express");
+const router = new express.Router();
+const invController = require("../controllers/invController");
+const utilities = require("../utilities");
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get(
+  "/type/:classificationId",
+  utilities.handleErrors(invController.buildByClassificationId),
+);
 
-// Vehicle detail route 
-router.get('/detail/:inv_id', utilities.handleErrors(invController.buildByInvId))
+// Vehicle detail route
+router.get(
+  "/detail/:inv_id",
+  utilities.handleErrors(invController.buildByInvId),
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,67 +1,70 @@
 /* ******************************************
- * This server.js file is the primary file of the 
+ * This server.js file is the primary file of the
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
-const static = require("./routes/static")
-const baseController = require("./controllers/baseController")
-const inventoryRoute = require("./routes/inventoryRoute")
-const utilities = require("./utilities/")
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const static = require("./routes/static");
+const baseController = require("./controllers/baseController");
+const baseRoute = require("./routes/baseRoute");
+const inventoryRoute = require("./routes/inventoryRoute");
+const utilities = require("./utilities/");
 
 /* ***********************
  * View Engine and Templates
  *************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout"); // not at views root
 
 /* ***********************
  * Routes
  *************************/
-app.use(express.static("public"))
+app.use(express.static("public"));
 
 // Index route
-app.get("/", utilities.handleErrors(baseController.buildHome))
+app.get("/", utilities.handleErrors(baseController.buildHome));
 
+// Base routes
+app.use("/", baseRoute);
 
 // Inventory routes
-app.use("/inv", inventoryRoute)
+app.use("/inv", inventoryRoute);
 
 //app.get("/", function(req, res) {
-  //res.render("index", {title: "Home"})
+//res.render("index", {title: "Home"})
 //})
 // File Not Found Route - must be last route in list
 app.use(async (req, res, next) => {
-  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
-})
+  next({ status: 404, message: "Sorry, we appear to have lost that page." });
+});
 
 /* ***********************
-* Express Error Handler
-* Place after all other middleware
-*************************/
+ * Express Error Handler
+ * Place after all other middleware
+ *************************/
 app.use(async (err, req, res, next) => {
-  let nav = await utilities.getNav()
-  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  let nav = await utilities.getNav();
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`);
 
-  let message
+  let message;
   if (err.status == 404) {
-    message = err.message
+    message = err.message;
   } else {
-    message = 'Oh no! There was a crash. Maybe try a different route?'
+    message = "Oh no! There was a crash. Maybe try a different route?";
   }
 
   res.status(err.status || 500).render("errors/error", {
-    title: err.status || 'Server Error',
+    title: err.status || "Server Error",
     message,
-    nav
-  })
-})
+    nav,
+  });
+});
 
 /* ***********************
  * Local Server Information
@@ -78,11 +81,9 @@ app.listen(port, host, () => {
 //const db = require('./database/db');
 
 //db.query('SELECT NOW()', (err, res) => {
-  //if (err) {
-    //console.error('Database connection failed:', err);
- // } else {
-    //console.log('Database connected at:', res.rows[0].now);
-  //}
+//if (err) {
+//console.error('Database connection failed:', err);
+// } else {
+//console.log('Database connected at:', res.rows[0].now);
+//}
 //});
-
-
